71c43446e84d889be07654a70d0984da59f5b21e
aesthetic0001 <15858616+aesthetic0001@users.noreply.github.com>
2024-11-18 15:32:16 -0500

    readd loaders test

--- tests/loaders.test.js
+++ tests/loaders.test.js
@@ -27,47 +27,46 @@ chunk.append(new Opcode("LOAD_OBJECT", 10, encodeArrayRegisters([]), encodeArray
 chunk.append(new Opcode("LOAD_OBJECT", 11, encodeArrayRegisters([6]), encodeArrayRegisters([4])));
 chunk.append(new Opcode("LOAD_OBJECT", 12, encodeArrayRegisters([6]), encodeArrayRegisters([11])));
 
-console.log(chunk.toString())
 const bytecode = chunk.toBytes().toString('base64')
 VM.loadFromString(bytecode, 'base64');
 VM.run()
-//
-// test('Load byte', () => {
-//     expect(VM.registers[3]).toBe(randByte);
-// })
-//
-// test('Load dword', () => {
-//     expect(VM.registers[4]).toBe(randInt);
-// })
-//
-// test('Load float', () => {
-//     expect(VM.registers[5]).toBe(randFloat);
-// })
-//
-// test('Load string', () => {
-//     expect(VM.registers[6]).toBe(randString);
-// })
-//
-// test('Load array (empty)', () => {
-//     expect(VM.registers[7]).toEqual([]);
-// })
-//
-// test('Load array (with values)', () => {
-//     expect(VM.registers[8]).toEqual([randByte, randInt, randFloat, randString, []]);
-// })
-//
-// test('Load array (nested)', () => {
-//     expect(VM.registers[9]).toEqual([[randByte, randInt, randFloat, randString, []], []]);
-// })
-//
-// test('Load object (empty)', () => {
-//     expect(VM.registers[10]).toEqual({});
-// })
-//
-// test('Load object (with values)', () => {
-//     expect(VM.registers[11]).toEqual({[randString]: randInt});
-// })
-//
-// test('Load object (nested)', () => {
-//     expect(VM.registers[12]).toEqual({[randString]: {[randString]: randInt}});
-// })
+
+test('Load byte', () => {
+    expect(VM.registers[3]).toBe(randByte);
+})
+
+test('Load dword', () => {
+    expect(VM.registers[4]).toBe(randInt);
+})
+
+test('Load float', () => {
+    expect(VM.registers[5]).toBe(randFloat);
+})
+
+test('Load string', () => {
+    expect(VM.registers[6]).toBe(randString);
+})
+
+test('Load array (empty)', () => {
+    expect(VM.registers[7]).toEqual([]);
+})
+
+test('Load array (with values)', () => {
+    expect(VM.registers[8]).toEqual([randByte, randInt, randFloat, randString, []]);
+})
+
+test('Load array (nested)', () => {
+    expect(VM.registers[9]).toEqual([[randByte, randInt, randFloat, randString, []], []]);
+})
+
+test('Load object (empty)', () => {
+    expect(VM.registers[10]).toEqual({});
+})
+
+test('Load object (with values)', () => {
+    expect(VM.registers[11]).toEqual({[randString]: randInt});
+})
+
+test('Load object (nested)', () => {
+    expect(VM.registers[12]).toEqual({[randString]: {[randString]: randInt}});
+})
