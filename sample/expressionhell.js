5f21951dbf260e79b6d86392dbd1968fe6c25cb1
aesthetic0001 <15858616+aesthetic0001@users.noreply.github.com>
2024-11-19 18:32:07 -0500

    change expression hell, prepare to fix literal spam issue

--- sample/expressionhell.js
+++ sample/expressionhell.js
@@ -3,7 +3,7 @@ const multiplier = 2;
 
 // @virtualize
 function evaluate(a, b) {
-    return (a + b - a + b * (a + b)) * (multiplier + (a * b) + (a + b));
+    return (a + b * ((a + b) * (a - b + (a + b * (multiplier + a + b)))));
 }
 
 console.log(evaluate(1, 2));
--- sample/expressionhell.virtualized.js
+++ sample/expressionhell.virtualized.js
@@ -2,13 +2,13 @@ const JSVM = require('../src/vm');
 const multiplier = 2;
 function evaluate(a, b) {
     const VM = new JSVM();
-    VM.loadFromString('1a6481a41b6464811a9681a41c96a4961a6464961c9681a41a961c961a9081a41a9696901c6464960f586426', 'hex');
+    VM.loadFromString('GoUkOhuxJDoaeaYkGnl5Ohx5OnkaeSR5GrGxeRyFhbEchTqFGoUkhQ+1hSY=', 'base64');
     VM.loadDependencies({
-        28: multiplier,
-        129: a,
-        164: b
+        36: a,
+        58: b,
+        166: multiplier
     });
     VM.run();
-    return VM.registers[88];
+    return VM.registers[181];
 }
 console.log(evaluate(1, 2));
