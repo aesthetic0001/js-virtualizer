c06290aae6ecfc7a47c12cabc7d8d9fc2128228c
aesthetic0001 <15858616+aesthetic0001@users.noreply.github.com>
2024-12-01 18:19:21 -0500

    :D

--- sample/test.js
+++ sample/test.js
@@ -2,7 +2,7 @@ const JSVM = require("../src/vm");
 
 // @virtualize
 function random() {
-    return Math.random().toString()
+    return Math.random().toString().slice(2, 5).split('').map(Number)
 }
 
 console.log(random())
--- sample/test.virtualized.js
+++ sample/test.virtualized.js
@@ -1,9 +1,12 @@
 const JSVM = require('../src/vm');
 function random() {
     const VM = new JSVM();
-    VM.loadFromString('eJxjFmdgYGArSsxLyc8VEf8mzj6PgYGBgTEATLqACE7xed/mMYOUcZTkB5cUZeali4jPE2dHKGFgTIAoDJgXIJQfoAUAjOQPGw==', 'base64');
-    VM.loadDependencies({ 246: Math });
+    VM.loadFromString('eJxVjT0OwjAMhR2iUrqiZGXpxsZZmDtUgFCk/qlk9Um4ng/gofKMkpJQFkvP+t73NALAfm6H+9gbbLBkAABF8Uo4FXLDOmAHP1797IanQcbyh4BaVpCYIli8Ond7GKRVt9tSqg4fy1KfRJaYipQqZOKvYeqczztqa9ChA5ZSJ63qvp3y5l/Dslyy//jm8weQOEwN', 'base64');
+    VM.loadDependencies({
+        49: Number,
+        93: Math
+    });
     VM.run();
-    return VM.registers[111];
+    return VM.registers[155];
 }
 console.log(random());
