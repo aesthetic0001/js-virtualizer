73aed5827f83728e30d4025ddfa9620fd4434d81
aesthetic0001 <15858616+aesthetic0001@users.noreply.github.com>
2024-12-03 09:36:01 -0500

    free all

--- sample/loops.virtualized.js
+++ sample/loops.virtualized.js
@@ -8,12 +8,12 @@ const a = [
 ];
 function evaluate() {
     const VM = new JSVM();
-    VM.loadFromString('eJyzyHloKZVj1SplLSWl1NrK38rAwBDPspCBgYE5Jz9dzEl5IQeIw8SkyQCifjAwMDCy7GVgYOB2yy9SyE9TyMnPLxBfqLlXRVPzh/hCTSkQze20UHkh3////1faAQBJQxpe', 'base64');
+    VM.loadFromString('789cb390e6b39c236da53dc77ace1c256d6d7e6d060686781625060606e69cfc74b1a6154a1c200e1313070388326760606064d9cec0c0c0ed965fa4909fa690939f5f20aec4b15d8583c35c5c89630e88e66e525aa1c4f7ffffff95760069d214e6', 'hex');
     VM.loadDependencies({
-        35: console,
-        225: a
+        14: a,
+        168: console
     });
     VM.run();
-    return VM.registers[141];
+    return VM.registers[196];
 }
 evaluate();
--- src/transformations/ForOfStatement.js
+++ src/transformations/ForOfStatement.js
@@ -32,6 +32,7 @@ function resolveForOfStatement(node) {
     if (needsCleanup(right)) this.freeTempLoad(iterator)
     if (iterator.borrowed) this.freeTempLoad(iteratorRegister)
     this.freeTempLoad(testRegister)
+    this.freeTempLoad(variableRegister)
 }
 
 module.exports = resolveForOfStatement
